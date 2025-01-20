7be3c7687ac70975f2630350958a3fdb4f2a3360
Ahumuza17 <ahumuzasamuel01gmail.com>
2025-01-20 11:50:43 +0300

    cherrypicking

--- src/factorial/factorial.js
+++ src/factorial/factorial.js
@@ -6,4 +6,6 @@ const factotial = (number) => {
     return number * factotial(number - 1);
   };
   console.log(factotial(10));
+  const power = (a, b)=>a**b;
+  const max= (a, b)=> Math.max(a, b)
 
